b175efeea248c883d99e23c54c0be9e80afb53a2
LOISGALLAUD <loisgallaud@hotmail.com>
2024-07-25 10:13:20 +0200

    saturate python icon

--- staturate.py
+++ staturate.py
@@ -3,30 +3,21 @@ import os
 from PIL import Image, ImageEnhance
 
 
-def saturate_images_in_directory(directory_path):
-    # Parcourt tous les fichiers dans le répertoire
-    for filename in os.listdir(directory_path):
-        if filename.endswith(".png"):
-            # Chemin complet du fichier
-            file_path = os.path.join(directory_path, filename)
+def saturate_images_in_directory(file_path):
+    with Image.open(file_path) as img:
+        # Convertir l'image en mode RGB si nécessaire
+        if img.mode != "RGB":
+            img = img.convert("RGB")
 
-            # Ouvrir l'image
-            with Image.open(file_path) as img:
-                # Convertir l'image en mode RGB si nécessaire
-                if img.mode != "RGB":
-                    img = img.convert("RGB")
+        # Appliquer la saturation maximale
+        enhancer = ImageEnhance.Color(img)
+        saturated_img = enhancer.enhance(3.0)  # 2.0 pour une saturation maximale
 
-                # Appliquer la saturation maximale
-                enhancer = ImageEnhance.Color(img)
-                saturated_img = enhancer.enhance(
-                    3.0
-                )  # 2.0 pour une saturation maximale
-
-                # Enregistrer l'image modifiée
-                saturated_img.save(file_path)
-                print(f"Image saturée et sauvegardée: {file_path}")
+        # Enregistrer l'image modifiée
+        saturated_img.save(file_path)
+        print(f"Image saturée et sauvegardée: {file_path}")
 
 
 # Remplacez 'votre_repertoire' par le chemin du répertoire contenant les images PNG
-directory_path = "icons_copy/"
-saturate_images_in_directory(directory_path)
+FILE = "icons/python.png"
+saturate_images_in_directory(FILE)
